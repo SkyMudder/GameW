2c4b0211cfb481d9869770d76f3be8c0500d6a52
Mud <skymudz@gmail.com>
2021-10-17 12:25:48 +0200

    World Objects can now be added to the Floor. Scale of Rocks still needs Adjustments.

--- Main.gd
+++ Main.gd
@@ -1,34 +1,41 @@
 extends Node2D
 
 
-var renderDistance: int = 5
+var renderDistance: int = 6
 
 var noise: OpenSimplexNoise
+var noiseObj: OpenSimplexNoise
 var chunks: Dictionary
 
 func _ready() -> void:
+	dir_contents("res://WorldObjects/")
 	$Player/Camera2D.current = true
+	#$Player/Camera2D.zoom = Vector2(5, 5)
 	noise = OpenSimplexNoise.new()
 	noise.octaves = 4
 	noise.period = 25
+	noiseObj = OpenSimplexNoise.new()
+	noiseObj.octaves = 7
+	noiseObj.period = 2
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	_setChunksRemove()
 	_checkGenerateChunk()
 	_checkRemoveChunks()
 
 func _checkGenerateChunk() -> void:
+	print(int($Player.position.x / 320 - renderDistance))
 	for x in range($Player.position.x / 320 - renderDistance, $Player.position.x / 320 + renderDistance):
 		for y in range($Player.position.y / 320 - renderDistance, $Player.position.y / 320 + renderDistance):
 			var key: String = str(x) + " " + str(y)
 			if !isChunkGenerated(key):
 				chunks[key] = _generateChunk(x, y)
-			chunks[key].remove = false
+			chunks[key].shouldRemove = false
 			
 
 func _checkRemoveChunks() -> void:
 	for x in chunks.keys():
-		if chunks[x].remove:
+		if chunks[x].shouldRemove:
 			var xy: Array = x.split(" ")
 			var wX: int = int(xy[0])
 			var wY: int = int(xy[1])
@@ -37,12 +44,12 @@ func _checkRemoveChunks() -> void:
 
 func _setChunksRemove():
 	for x in chunks.keys():
-		chunks[x].remove = true
+		chunks[x].shouldRemove = true
 
 func _generateChunk(x: int, y: int) -> Node2D:
 	var chunk = Chunk.new()
 	$Chunks.add_child(chunk)
-	chunk.generate(noise, x, y)
+	chunk.generate(noise, noiseObj, x, y)
 	return chunk
 
 func isChunkGenerated(key: String) -> bool:
@@ -50,3 +57,18 @@ func isChunkGenerated(key: String) -> bool:
 		if x == key:
 			return true
 	return false
+
+func dir_contents(path):
+	var dir = Directory.new()
+	if dir.open(path) == OK:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+			else:
+				if ".tscn" in file_name:
+					print("Found file: " + file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
--- WorldGen/Chunk.gd
+++ WorldGen/Chunk.gd
@@ -6,33 +6,55 @@ onready var background = get_parent().get_node("Background")
 
 const CHUNK_SIZE: int = 10
 
-var remove = false
+var tree: PackedScene = preload("res://WorldObjects/Tree.tscn")
+var rock: PackedScene = preload("res://WorldObjects/Rock.tscn")
 
-func generate(noise: OpenSimplexNoise, wX: float, wY: float) -> void:
+var shouldRemove: bool = false
+var ySort: YSort
+
+func _ready() -> void:
+	ySort = YSort.new()
+	add_child(ySort)
+
+func generate(noise: OpenSimplexNoise, noiseObj: OpenSimplexNoise, wX: float, wY: float) -> void:
 	for x in range(CHUNK_SIZE):
 		for y in range(CHUNK_SIZE):
-			_generateTile(noise, x + wX * CHUNK_SIZE, y + wY * CHUNK_SIZE)
-	for x in range(CHUNK_SIZE):
-		for y in range(CHUNK_SIZE):
-			ground.update_bitmask_area(Vector2(x + wX * CHUNK_SIZE, y + wY * CHUNK_SIZE))
+			var noise2D: float = noise.get_noise_2d(x + wX * CHUNK_SIZE, y + wY * CHUNK_SIZE)
+			var noise2DObj: float = noiseObj.get_noise_2d(x + wX * CHUNK_SIZE, y + wY * CHUNK_SIZE)
+			_generateFloor(noise2D, Vector2(x + wX * CHUNK_SIZE, y + wY * CHUNK_SIZE))
+			_generateObjects(noise2D, noise2DObj, Vector2(x * 32 + wX * 320, y * 32 + wY * 320))
 
-func _generateTile(noise: OpenSimplexNoise, x: float, y: float) -> void:
-	var tmpNoise: float = noise.get_noise_2d(x, y)
-	if tmpNoise <= 0:
-		ground.set_cell(x, y, 0)
-		if tmpNoise > -0.1:
-			background.set_cell(x, y, 0)
+func _generateFloor(noise2D: float, position: Vector2) -> void:
+	_generateTile(noise2D, position)
+	ground.update_bitmask_area(position)
+
+func _generateTile(noise2D: float, position: Vector2) -> void:
+	if noise2D <= 0:
+		ground.set_cellv(position, 1)
+		if noise2D > -0.1:
+			background.set_cellv(position, 1)
 	else:
-		ground.set_cell(x, y, 1)
-		if tmpNoise < 0.1:
-			background.set_cell(x, y, 1)
+		ground.set_cellv(position, 0)
+		if noise2D < 0.1:
+			background.set_cellv(position, 0)
 
-func _removeTile(x: int, y: int) -> void:
-	ground.set_cell(x, y, -1)
+func _generateObjects(noise2D: float, noise2DObj: float, position: Vector2) -> void:
+	if noise2D > 0 and noise2DObj > 0.35:
+		_instantiateObject(tree, position)
+	elif noise2D < 0 and noise2DObj < -0.35:
+		_instantiateObject(rock, position)
+
+func _removeTile(position: Vector2) -> void:
+	ground.set_cellv(position, -1)
+	background.set_cellv(position, -1)
 
 func remove(wX: int, wY: int) -> void:
-	var a = 0
 	for x in range(CHUNK_SIZE):
 		for y in range(CHUNK_SIZE):
-			_removeTile(x + wX * CHUNK_SIZE, y + wY * CHUNK_SIZE)
+			_removeTile(Vector2(x + wX * CHUNK_SIZE, y + wY * CHUNK_SIZE))
 	self.queue_free()
+
+func _instantiateObject(Obj: PackedScene, position: Vector2) -> void:
+	var object: StaticBody2D = Obj.instance()
+	object.position = position
+	ySort.add_child(object)
